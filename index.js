83528a2d79b3fc4992e9eb3c495cbc1e327108df
Anas Shahid <muhammad.shahid@mail.mcgill.ca>
2020-02-02 06:01:41 -0500

    second draft

--- index.js
+++ index.js
@@ -2,50 +2,58 @@ const curriculum = [
 	{
 		Title: 'First Assignment',
 		Description: 'desc',
-		Image: './assets/screenshot.png',
+        Image: './assets/screenshot.png',
+        URL: 
 	},
 
 	{
 		Title: 'Second Assignment',
 		Description: 'desc',
-		Image: './assets/screenshot.png',
+        Image: './assets/screenshot.png',
+        URL:
 	},
 
 	{
 		Title: 'Third Assignment',
 		Description: 'desc yolo',
-		Image: './assets/hideandseek2.png',
+        Image: './assets/hideandseek2.png',
+        URL: ''
 	},
 
 	{
 		Title: 'Fourth Assignment',
 		Description:
 			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque blanditiis eum quibusdam officia illum, quo perferendis illo esse explicabo deserunt.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque blanditiis eum quibusdam officia illum, quo perferendis illo esse explicabo deserunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo eius exercitationem recusandae nobis minus. Dicta eos blanditiis, distinctio consequuntur qui quod quis enim iusto cumque doloremque? Exercitationem odit at quidem?',
-		Image: './assets/screenshot.png',
+        Image: './assets/screenshot.png',
+        URL: ''
 	},
 
 	{
 		Title: 'Fifth Assignment',
 		Description: 'desc',
-		Image: './assets/screenshot.png',
+        Image: './assets/screenshot.png',
+        URL: ''
 	},
 
 	{
 		Title: 'Sixth Assignment',
 		Description: 'desc',
-		Image: './assets/screenshot.png',
+        Image: './assets/screenshot.png',
+        URL: ''
 	},
 
 	{
 		Title: 'Seventh Assignment',
 		Description: 'desc',
-		Image: './assets/screenshot.png',
+        Image: './assets/screenshot.png',
+        URL: ''
 	},
 
 	{
 		Title: 'Eigth Assignment',
 		Description: 'desc',
-		Image: './assets/screenshot.png',
+        Image: './assets/screenshot.png',
+        URL: ''
 	},
 ];
 
@@ -53,7 +61,8 @@ const next = document.querySelector('.next');
 const title = document.querySelector('.title');
 const description = document.querySelector('.desc');
 const image = document.querySelector('#ss-image');
-var count = 0;
+const lesson = document.querySelector('#lesson');
+var count = 1;
 next.addEventListener('click', () => {
 	if (count >= curriculum.length) {
 		count = count % curriculum.length;
@@ -63,4 +72,7 @@ next.addEventListener('click', () => {
 	image.src = curriculum[count].Image;
 	console.log(count);
 	count++;
+});
+lesson.addEventListener('click', () => {
+	window.location.href = curriculum[count].URL;
 });
