2ddd760d8562a74773a071460012bcb6f9d15185
Anas Shahid <muhammad.shahid@mail.mcgill.ca>
2020-02-03 19:41:57 -0500

    added cat family to the UI/UX

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-const curriculum = [
-	{
+const curriculum = [{
 		Title: 'Dancing Array',
-		Description:
-			'Teach Coda how to dance with arrays! Arrays are list of things, in this case list of dance moves. We taught Coda a few, see if you can use them',
+		Description: 'Teach Coda how to dance with arrays! Arrays are list of things, in this case list of dance moves. We taught Coda a few, see if you can use them',
 		Image: './assets/dancingArray.PNG',
 		URL: 'https://www.openprocessing.org/sketch/833648/',
 	},
 
 	{
 		Title: 'Hide and Seek',
-		Description:
-			'Quick, we need to hide Coda!First, lets look at the array of hiding spots and hide our favourite cat at the first one we see',
+		Description: 'Quick, we need to hide Coda!First, lets look at the array of hiding spots and hide our favourite cat at the first one we see',
 		Image: './assets/hidenSeek.PNG',
 		URL: 'https://www.openprocessing.org/sketch/833642/',
 	},
 
 	{
 		Title: 'For Loops',
-		Description:
-			'This time you are going to learn about the for loop! For loops repeat actions over and over, and contain 3 parts -- The first section declares a variable, the second part sets the rules, and the third part updates the variable.',
+		Description: 'This time you are going to learn about the for loop! For loops repeat actions over and over, and contain 3 parts -- The first section declares a variable, the second part sets the rules, and the third part updates the variable.',
 		Image: './assets/forLoops.PNG',
 		URL: 'https://www.openprocessing.org/sketch/833604',
 	},
@@ -32,8 +28,7 @@ const curriculum = [
 
 	{
 		Title: 'CodaCat 101',
-		Description:
-			'But where is he? Write this line of code in the main function and press the play button at the top to make him appear :)',
+		Description: 'But where is he? Write this line of code in the main function and press the play button at the top to make him appear :)',
 		Image: './assets/screenshot.png',
 		URL: 'https://www.openprocessing.org/sketch/833647',
 	},
@@ -59,6 +54,7 @@ const description = document.querySelector('.desc');
 const image = document.querySelector('#ss-image');
 const lesson = document.querySelector('#lesson');
 var count = 0;
+
 next.addEventListener('click', () => {
 	if (count >= curriculum.length - 1) {
 		count = count % curriculum.length;
@@ -80,4 +76,52 @@ lesson.addEventListener('click', () => {
 		window.location.href = curriculum[lessonCounter].URL;
 		console.log('else', count, lessonCounter);
 	}
+});
+
+// doing it for the cats
+
+const assets = [
+	'./assets/cheer.png',
+	'./assets/dab.png',
+	'./assets/fancy.png',
+	'./assets/friend3yay.png',
+	'./assets/friendsit.png',
+	'./assets/yarnsit.png',
+	'./assets/yarnyay.png',
+	'./assets/yayfriend.png',
+	'./assets/yeah.png',
+	'./assets/hideandseek1.png',
+	'./assets/hideandseek2.png',
+	'./assets/hideandseek3.png',
+	'./assets/hideandseek4.png',
+	'./assets/hideandseek5.png',
+]
+const before = document.querySelector('.previous');
+const after = document.querySelector('.after');
+const currentCat = document.querySelector('.cats');
+var catCounter = 0;
+// window.onload(() => currentCat.src = assets[0]);
+window.onload = () => {
+	currentCat.src = assets[0];
+};
+
+console.log("hello im here");
+after.addEventListener('click', () => {
+	if (catCounter >= assets.length) {
+		catCounter = catCounter % assets.length;
+	} else if (catCounter == assets.length - 1) {
+		catCounter.src = assets[catCounter];
+		catCounter = -1;
+	} else {
+		catCounter++;
+		currentCat.src = assets[catCounter];
+	}
+});
+
+before.addEventListener('click', () => {
+	if (catCounter <= 0) {
+		catCounter = assets.length - 1;
+	}
+	catCounter--;
+	currentCat.src = assets[catCounter];
 });
